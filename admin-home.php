36e78c964461e6f2598df29a4523bf9f2a876be5
pratyushjain99 <73490165+pratyushjain99@users.noreply.github.com>
2020-11-15 22:23:14 +0530

    FINAL
    FINAL

---
+++ admin-home.php
@@ -0,0 +1,39 @@
+<?php
+include('connection.php')
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin Login</title>
+	<link rel="stylesheet" type="text/css" href="css/s1.css">
+</head>
+<body>
+<div id="full">
+    <div id="inner_full">
+        <div id="header"><h2><a href="admin-home.php" style="text-decoration:none;color:white;">DBMS for a Blood Bank</a></h2></div>
+        <div id="body">
+            <br><br>
+            <h1>WELCOME ADMIN</h1><br><br>
+
+            <ul>
+                <li><a href="donor-red.php">Donor Registration</a></li>
+                <li><a href="donor-list.php">Donor List</a></li>
+                <li><a href="stock-blood-list.php">Blood Stock Available</a></li>
+            </ul><br><br><br><br><br><br>
+            <ul>
+                
+                 <li><a href="exchange-blood-list.php">Recipient Registration</a></li>
+                <li><a href="Recipient-list.php">Recipient List</a></li>
+                 
+            
+            </ul>
+        </div>
+        <div id="footer"><hd align="center">Made by Abhinav, Pratyush and Sarthak</h4>
+            <p align ="center"> <a href="logout.php"><font color="white">Logout</font></a></p>
+        
+        </div>
+    </div>
+</div>
+</body>
+</html>
+   
